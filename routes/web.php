a2755348e688b42b8eb0376b883b68d10f899407
christofer-munoz <xXkuakXx@gmail.com>
2020-10-30 01:52:03 -0300

    avance

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,29 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+php artisan route:list
+php artisan route:list -c
+php artisan route:list --path=products
+php artisan route:list --path=products -r
 */
 
 Route::get('/', function () {
+    return redirect("categories");
     return view('welcome');
+});
+
+Route::get('/products', function() {
+    return "<h1>All Products</h1>";
+});
+
+Route::get('/products/create', function() {
+    return "<h1>Add new product</h1>";
+});
+
+Route::get('/products/create/{product}', function($product) {
+    return "<h1>Product {$product}</h1>";
+});
+
+Route::get('/categories', function() {
+    return response()->json(["Category 1", "Category 2"]);
 });
